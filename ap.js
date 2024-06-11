2e40158d340585c0444f3f6d850553c47b947303
Minimvm X <167718981+Guene72@users.noreply.github.com>
2024-06-11 14:09:50 +0000

    Update ap.js

--- ap.js
+++ ap.js
@@ -7,10 +7,8 @@ const mysql = require('mysql2');
 const sessions = require('express-session');
 const bodyParser = require('body-parser');
 const path = require('path');
-
 const app = express();
 const { error, log } = require('console');
-
 app.set('view engine', 'ejs');
 app.set('views', 'GESTION');
 
@@ -22,14 +20,8 @@ const connexion = mysql.createConnection({
     password: '',
     database: 'pigier'
   });
+
   
-  
-  // Creation de la session
-  app.use(sessions({
-    secret: 'La rue',
-    resave: false,
-    saveUninitialized: true,
-  }));
   
   // Connection à la base de données
   connexion.connect((err) => {
@@ -42,24 +34,24 @@ const connexion = mysql.createConnection({
 
 app.get('/login',(req, res)=>{
         // const { username, password } = req.body;
-       // let  username = 'koli'
-       // let  password = '123456'
+       let  username = 'koli'
+       let  password = '123456'
           // Si l'utilisateur est un étudiant 
           connexion.query(`SELECT * FROM pigier.etudiant WHERE userame = '${username}' AND password = '${password}'`, (error, result) => {
             if (error) console.log('Erreur de connexion à etudiant //');
             if (result.length > 0) {  
               // page de redirection pour unne bonne connexiion
-              // res.json({etudiant: 'connexion effectuer'});
+              res.json({etudiant: 'connexion effectuer'});
             }else{
               // Si l'utilisateur est un admin
               connexion.query(`SELECT * FROM admin WHERE username = '${username}' AND password = '${password}'`, (error, result) => {
-                if (error) console.log('Erreur sur la connexion du professeur');
+                if (error) console.log('Erreur sur la connexion du professeur',error);
                 if (result.length) {
                   // Page de redirection pour une bonne connexion                
-                  // res.json({etudiant:result});                    
+                  res.json({etudiant:result});                    
                 } else {
                   // Sinon tu le redirige s'il nest ni etudiant et ni admin
-                  res.json({etudiant:'erreur de connexion au deux personnes'})
+                  res.json({encadreur:'erreur de connexion au deux personnes'})
                 }
               });
              }
@@ -75,26 +67,62 @@ app.get('/demandes',(req, res)=>{
   let datenaiss = '2022-10-25'
   let username = 'Nfk'
   let password = '123456'
-  let filiere = 'RGL'
+  let filiere = 'assistante de direction'
   let themeSoutenance = 'Gestion d\'un magasin'
-  const query2 = 'INSERT INTO filiere (nomfiliere) VALUES (?)';
-  const value2 = [filiere]
 
-  connexion.query(query2, value2, (error, result)=>{
-    if (error) {
-      console.log('Erreur d\'ajout dans la base de donnée', error);
-    }else{
-      const idEt = result.insertId ;
-      const query1 = 'INSERT INTO etudiant (nom, prenoms, datenaiss, userame, password, themesoutenance, idfiliere ) VALUES (?, ?, ?, ?, ?, ?, ?)'
-      const value1 = [nom, prenoms, datenaiss, username, password, themeSoutenance, idEt] 
-      connexion.query(query1, value1, (error, result)=>{
-        if (error) {
-          console.log('Erreur d\'ajout de la demande', error);  
-        }else
-        res.json({resultat: result})
-      })
-    }
-  })
+      if(filiere === 'RGL'){
+        const query1 = 'INSERT INTO etudiant (nom, prenoms, datenaiss, userame, password, themesoutenance, idfiliere ) VALUES (?, ?, ?, ?, ?, ?, ?)'
+        const value1 = [nom, prenoms, datenaiss, username, password, themeSoutenance, 1] 
+        connexion.query(query1, value1, (error, result)=>{
+          if (error) {
+            console.log('Erreur d\'ajout de la demande', error);  
+          }else
+          res.json({resultat: result})
+        })
+      }else if(filiere ==='comptabilite'){
+        const query1 = 'INSERT INTO etudiant (nom, prenoms, datenaiss, userame, password, themesoutenance, idfiliere ) VALUES (?, ?, ?, ?, ?, ?, ?)'
+        const value1 = [nom, prenoms, datenaiss, username, password, themeSoutenance, 2] 
+        connexion.query(query1, value1, (error, result)=>{
+          if (error) {
+            console.log('Erreur d\'ajout de la demande', error);  
+          }else
+          res.json({resultat: result})
+        })
+
+      }else if(filiere ==='commerciale'){
+        const query1 = 'INSERT INTO etudiant (nom, prenoms, datenaiss, userame, password, themesoutenance, idfiliere ) VALUES (?, ?, ?, ?, ?, ?, ?)'
+        const value1 = [nom, prenoms, datenaiss, username, password, themeSoutenance, 3] 
+        connexion.query(query1, value1, (error, result)=>{
+          if (error) {
+            console.log('Erreur d\'ajout de la demande', error);  
+          }else
+          res.json({resultat: result})
+        })
+       }else if(filiere ==='ressource humaine'){
+        const query1 = 'INSERT INTO etudiant (nom, prenoms, datenaiss, userame, password, themesoutenance, idfiliere ) VALUES (?, ?, ?, ?, ?, ?, ?)'
+        const value1 = [nom, prenoms, datenaiss, username, password, themeSoutenance, 4] 
+        connexion.query(query1, value1, (error, result)=>{
+          if (error) {
+            console.log('Erreur d\'ajout de la demande', error);  
+          }else
+          res.json({resultat: result})
+        })
+       }else if(filiere ==='assistante de direction'){
+        const query1 = 'INSERT INTO etudiant (nom, prenoms, datenaiss, userame, password, themesoutenance, idfiliere ) VALUES (?, ?, ?, ?, ?, ?, ?)'
+        const value1 = [nom, prenoms, datenaiss, username, password, themeSoutenance, 5] 
+        connexion.query(query1, value1, (error, result)=>{
+          if (error) {
+            console.log('Erreur d\'ajout de la demande', error);  
+          }else{
+            connexion.query('SELECT nom, prenoms, themesoutenance, nomfiliere FROM etudiant JOIN filiere ON etudiant.idfiliere = filiere.idfiliere',(error, results)=>{
+              if(error) console.log(error);
+              else res.json({resultats: results})
+            }
+            )
+          }
+          
+        })
+       }
 })
 
 app.use('/etudiant', route);
